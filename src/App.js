3acdd05e29232823a75495dfb79629c49656f45f
Jooyoung Oh <jooyoungoh@bba389451.alshamil.net.ae>
2022-09-01 03:49:08 +0400

    Add Header.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 
 function App() {
-  return (
-      // BEM
-    <div className="app">
-      <h1>Let's build the Amazon store</h1>
-    </div>
-  );
+    return (
+        // BEM
+        <div className="app">
+            <h1>Let's build the Amazon store</h1>
+            {/* Header */}
+            {/* Home */}
+        </div>
+    );
 }
 
 export default App;
