a3d7caffd188bff019c837aad51168476cb1247c
Jooyoung Oh <jooyoungoh@bba389451.alshamil.net.ae>
2022-09-01 03:32:48 +0400

    Configure firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC0NouHX9XZ8vY-CSD7RWDIDCOVvskv5Wk",
+    authDomain: "clone-9c0bd.firebaseapp.com",
+    projectId: "clone-9c0bd",
+    storageBucket: "clone-9c0bd.appspot.com",
+    messagingSenderId: "700338699238",
+    appId: "1:700338699238:web:945d4fb86e51dfc2fb7427",
+    measurementId: "G-TYL6JKWGM5"
+};
