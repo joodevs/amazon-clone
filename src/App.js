0a3800cab5cfc2971d9e5fa025698cad9154c835
Jooyoung Oh <jooyoungoh@bba389451.alshamil.net.ae>
2022-09-01 05:35:13 +0400

    Configure header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import React from "react";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
     return (
         // BEM
         <div className="app">
             <h1>Let's build the Amazon store</h1>
-            {/* Header */}
+            <Header />
             {/* Home */}
         </div>
     );
--- src/Header.js
+++ src/Header.js
@@ -2,11 +2,46 @@ import React from "react";
 import "./Header.css";
 
 function Header() {
-  return (
-    <div className="header">
-      <img className="header__logo" src="src/assets/amazon-logo.png" />
-    </div>
-  );
+    return (
+        <div className="header">
+            <img className="header__logo"
+                 src="src/assets/amazon-logo.png"/>
+            <div
+                className="header__search">
+                <input
+                    className="header__searchInput"
+                    type="text"/>
+                {{/* Logo */}}
+            </div>
+
+            <div className="header__nav">
+                <div className="header__option">
+                    <span
+                        className='header__optionLineOne'>
+                        Hello Guest</span>
+                    <span
+                        className='header__optionLineTwo'>
+                        Sign In</span>
+                </div>
+                <div className="header__option">
+                    <span
+                        className='header__optionLineOne'>
+                        Returns</span>
+                    <span
+                        className='header__optionLineTwo'>
+                        & Orders</span>
+                </div>
+                <div className="header__option">
+                    <span
+                        className='header__optionLineOne'>
+                        Your</span>
+                    <span
+                        className='header__optionLineTwo'>
+                        Prime</span>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Header;
