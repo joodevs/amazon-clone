ba13fcc8a431d942eda9099e0424f21fa971a2b8
Jooyoung Oh <jooyoungoh@bba389451.alshamil.net.ae>
2022-09-01 04:18:48 +0400

    Configure header

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <img className="header__logo" src="src/assets/amazon-logo.png" />
+    </div>
+  );
+}
+
+export default Header;
